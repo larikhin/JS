1f849aa39c6579efba8458659f3419fc630e8e46
VL <larikhin@gmail.com>
2023-09-01 10:40:31 +0300

    Add files via upload

--- movements/js/script.js
+++ movements/js/script.js
@@ -4,30 +4,45 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 500;
 const CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 10;
+const numberOfEnemies = 30;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = "./images/1.png";
+
+let gameFrame = 0;
 
 class Enemy {
 	constructor(){
-		// стартовые координаты врага
-		this.x = Math.random() * canvas.width;
-		this.y = Math.random() * canvas.height;
-		// размеры врага
-		this.width = 100;
-		this.height = 100;
+		this.image = new Image();
+		this.image.src = "./images/enemy1.png";
 		// скорость врага
-		this.speed = Math.random() * 4 -2;
+		//this.speed = Math.random() * 4 -2;
+		// указание размера одного спрайта
+		this.spriteWidth = 293;
+		this.spriteHeight = 155;
+		// размеры врага
+		this.width = this.spriteWidth/3;
+		this.height = this.spriteHeight/3;
+		// стартовые координаты врага, (...) для ограничения размерами экрана
+		this.x = Math.random() * (canvas.width - this.width);
+		this.y = Math.random() * (canvas.height - this.height);
+		this.frame = 0;
+		// случайная смена спрайтов, рассинхрон одинаковых
+		this.flapSpeed = Math.floor(Math.random() * 3 + 1);
 	}
 	update(){	// обновление координат
-		this.x += this.speed;
-		this.y += this.speed;
+		this.x += Math.random()*15-7.5; // цифры меняют разлет случайного числа
+		this.y += Math.random()*10-5;
+		// замедление анимации, каждые 2е смены картинки
+		if (gameFrame % this.flapSpeed===0){
+		// анимация спрайтов
+		// если this.frame > 4 ? то  this.frame = 0 : иначе this.frame++;
+		this.frame > 4 ? this.frame = 0 : this.frame++;}
 	}
 	draw(){		//отрисовка фигуры
 		// ctx.strokeRect(this.x, this.y, this.width, this.height);
-		ctx.drawImage(enemyImage, this.x, this.y);
+		// (название картинки, 2 откуда, 2 размер, 2 куда, 2 масшатб)
+		ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight,
+									 this.x, this.y, this.width, this.height);
 	}
 };
 
@@ -41,6 +56,7 @@ function animate(){
 		enemy.update();
 		enemy.draw();
 		});
+	gameFrame++;
 	requestAnimationFrame(animate);
 }
 animate()
